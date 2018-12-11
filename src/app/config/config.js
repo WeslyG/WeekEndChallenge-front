6a2e0aef543f17aa450468ac1ae0a0d27e2239a9
WeslyG <Weslyg22@gmail.com>
2018-12-11 10:50:15 +0500

    Режу верстку тима 10 часов, для сонара

--- src/app/config/config.js
+++ src/app/config/config.js
@@ -1,8 +1,8 @@
 export default {
     backend: {
         // backEndUrl: 'http://46.163.163.75:3000',
-        backEndUrl: 'https://wc.weslyg.ru/api',
-        // backEndUrl: 'http://localhost:3000/api',
+        // backEndUrl: 'https://wc.weslyg.ru/api',
+        backEndUrl: 'http://localhost:3000/api',
         apiVersion: ''
     },
     elasticsearch: {
