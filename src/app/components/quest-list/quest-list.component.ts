9db124c8a819df2087d2a1ab5da4c8933535d967
zakriev <ztf@datakrat.ru>
2018-12-12 16:23:05 +0500

    Адаптивная верстка и обработка логина пользователя.

--- src/app/components/quest-list/quest-list.component.ts
+++ src/app/components/quest-list/quest-list.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { QuestListService } from '../../services/questList.service';
 import { MatSnackBar, MatDialog } from '@angular/material';
 import { Response } from '@angular/http';
+import { LocalStorageService } from 'src/app/services/localStorage.service';
+import { DbKeys } from 'src/app/services/db-keys.service';
 
 @Component({
   selector: 'app-quest-list',
@@ -10,25 +12,36 @@ import { Response } from '@angular/http';
 })
 export class QuestListComponent implements OnInit {
 
+  private isUserLogin: Boolean = false;
+
   constructor(
     private questListServices: QuestListService,
-    private snackBar: MatSnackBar
-  ) { }
+    private snackBar: MatSnackBar,
+    private localStorageService: LocalStorageService,
+  ) {}
 
   isLoading = false;
   questList = [];
 
   ngOnInit() {
     this.isLoading = true;
+    this.isUserLogin = this.isLogin();
     this.questListServices.getQuestList().subscribe(res => {
-      this.isLoading = false;
-      this.questList = res;
-    },
+        this.isLoading = false;
+        this.questList = res;
+      },
       (error: Response) => {
         this.isLoading = false;
         this.snackBar.open(error.text(), 'Ok', {
           duration: 3000,
         });
       });
+  }
+  private isLogin() {
+    if (this.localStorageService.getDataFromStorage(DbKeys.ID_TOKEN)) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
